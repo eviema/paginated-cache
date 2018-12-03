c00984d39ccd59a88b16fc033bbbfe32f4046352
Evie Ma <yfma89@gmail.com>
2018-12-03 14:09:25 +1100

    minor change in styling

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,12 +1,14 @@
 import React from "react";
 import img from "../assets/loading_spinner.gif";
 
+const style = {
+  textAlign: "center",
+  height: "100%",
+  width: "100%"
+};
 export default () => {
   return (
-    <div
-      style={{ textAlign: "center", height: "100%", width: "100%" }}
-      id="loading"
-    >
+    <div style={style} id="loading">
       <img src={img} alt="loading" />
       <h1>LOADING</h1>
     </div>
--- src/containers/ErrorPage.js
+++ src/containers/ErrorPage.js
@@ -2,12 +2,14 @@ import React from "react";
 import PropTypes from "prop-types";
 import img from "../assets/cloud_off.png";
 
+const style = {
+  textAlign: "center",
+  height: "100%",
+  width: "100%"
+};
 export default function ErrorPage({ error }) {
   return (
-    <div
-      style={{ textAlign: "center", height: "100%", width: "100%" }}
-      id="error"
-    >
+    <div style={style} id="error">
       <img src={img} alt="error" />
       <h1>{error}</h1>
     </div>
